ad4c456b9782d1ddf362f156e9383cc2a1f39051
michalaziz <michalaziz1@gmail.com>
2019-04-07 15:23:18 +0300

    start test1

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -62,20 +62,48 @@ int main()
         .CHECK_OUTPUT((a += PhysicalNumber(1, Unit::TON)), "1700[kg]")
 
         // YOUR TESTS - INSERT AS MANY AS YOU WANT
-        .CHECK_OUTPUT(l1 + l2, "8[km]")
-        .CHECK_OUTPUT(l2 + l1, "8000[m]")
-        .CHECK_OUTPUT(l1 + l3, "4.00004[km]")
-        .CHECK_OUTPUT(l3 + l1, "400004[cm]")
-        .CHECK_OUTPUT(l2 + l3, "40000.04[m]")
-        .CHECK_OUTPUT(l3 + l2, "400004[cm]")
-        .CHECK_OUTPUT(t1 + t2, "420[sec]")
-        .CHECK_OUTPUT(t2 + t1, "7[min]")
-        .CHECK_OUTPUT(t1 + t3, "10860[sec]")
-        .CHECK_OUTPUT(t3 + t1, "3.01[hour]")
-        .CHECK_OUTPUT(t2 + t3, "186[min]")
-        .CHECK_OUTPUT(t3 + t2, "3.06[hour]")
-        
 
+        //+ operator tests
+        .CHECK_OUTPUT(l1 + l2, "8[km]")//m to km 
+        .CHECK_OUTPUT(l1 + l1, "8[km]")
+        .CHECK_OUTPUT(l2 + l1, "8000[m]")//km to m 
+        .CHECK_OUTPUT(l1 + l3, "4.00004[km]")//cm to km
+        .CHECK_OUTPUT(l3 + l1, "400004[cm]")//km to cm 
+        .CHECK_OUTPUT(l2 + l3, "40000.04[m]")//cm to m 
+        .CHECK_OUTPUT(l3 + l2, "400004[cm]")//m to cm 
+        .CHECK_OUTPUT(t1 + t2, "420[sec]")// min to sec 
+        .CHECK_OUTPUT(t2 + t1, "7[min]")//sec to min 
+        .CHECK_OUTPUT(t1 + t3, "10860[sec]")// hour to sec 
+        .CHECK_OUTPUT(t3 + t1, "3.001[hour]")// sec to hour
+        .CHECK_OUTPUT(t2 + t3, "186[min]")// hour to min 
+        .CHECK_OUTPUT(t3 + t2, "3.06[hour]")// min to hour 
+        .CHECK_THROWS(t1 + l2)
+        .CHECK_THROWS(w1 + l2)
+
+        //- operator tests
+        .CHECK_OUTPUT(l1 - l2, "0[km]")      //m to km
+        .CHECK_OUTPUT(l2 - l1, "0[m]")       //km to m
+        .CHECK_OUTPUT(l2 - l3, "3999.96[m]") //cm to m
+        .CHECK_THROWS(l3 - l2)               //m to cm
+        .CHECK_THROWS(l3 - l1)               //km to cm
+        .CHECK_OUTPUT(l1 - l3, "3.99996")    //cm to km
+        .CHECK_THROWS(t1 - l2)
+        .CHECK_THROWS(w1 - l2)
+
+        //operator +=
+        .CHECK_OUTPUT((w2 += w1), "2.3[kg]")// g to kg 
+        .CHECK_OUTPUT((w3+= w1), "2.3[ton]")//g to ton 
+        .CHECK_OUTPUT((w1 += w2), "2.3[g]")//kg to g
+        .CHECK_OUTPUT((w3 += w2), "2.3[ton]")//kg to ton
+        .CHECK_OUTPUT((w2 += w3), "2.3[kg]")//ton to kg
+        .CHECK_OUTPUT((w1 += w3), "2.3[g]")//ton to g
+        .CHECK_THROWS(w1 += l2)
+        .CHECK_THROWS(t1 += w2)
+        .CHECK_THROWS(l1 += w3)
+        .CHECK_THROWS(w2 += t2)
+        
+        //-= operator
+        
 
 
         .setname("...")
